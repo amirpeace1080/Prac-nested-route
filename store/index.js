11f6f830dc42d87a949bb5e11715ebe48a4cf229
amirpeace1080 <amirpeace1080@gmail.com>
2021-01-12 16:48:39 +0330

    first commit

---
+++ store/index.js
@@ -0,0 +1,11 @@
+import Vuex from 'vuex'
+import Promise from 'core-js/features/promise'
+
+export default () =>{
+    return new Vuex.Store({
+        state: {},
+        mutations: {},
+        actions: {},
+        getters: {}
+    })
+}
